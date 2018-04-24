8fc9990009f3ed97ab4357d596c89f16b7c9eda1
Lawrence Chen <lawrencechen2002@gmail.com>
2018-04-23 20:20:25 -0700

    Refactor code

--- app.js
+++ app.js
@@ -1,37 +1,75 @@
 const square = document.getElementById('square');
 let squarePos = {
-  x: 0,
-  y: 0
-};
+  x: 300,
+  y: 300
+}
+let sideLength = 100;
 let isDragging = false;
 
 square.style.left = squarePos.x + 'px';
 square.style.top = squarePos.y + 'px';
 
-let dragSquare = (mouseX, mouseY) => {
-    let offsetX = mouseX - squarePos.x;
-    let offsetY = mouseY - squarePos.y;
-
-    let x = mouseX - offsetX;
-    let y = mouseY - offsetY;
-  
-    square.style.left = x + 'px';
-    square.style.top = y + 'px';
-  
+let bodyWidth = () => {
+  return Math.max(
+    document.body.scrollWidth,
+    document.documentElement.scrollWidth,
+    document.body.offsetWidth,
+    document.documentElement.offsetWidth,
+    document.documentElement.clientWidth
+  );
 }
 
-square.onclick = () => {
-  isDragging = !isDragging;
-  console.log(isDragging);
+let bodyHeight = () => {
+  return Math.max(
+    document.body.scrollHeight,
+    document.documentElement.scrollHeight,
+    document.body.offsetHeight,
+    document.documentElement.offsetHeight,
+    document.documentElement.clientHeight
+  );
 }
 
-square.onmouseup = () => {
-  isDragging = !isDragging;
-  console.log(isDragging);
+square.onmousedown = (e) => {
+  isDragging = true;
+  let mx = e.clientX - squarePos.x;
+  let my = e.clientY - squarePos.y;
+  square.style.cursor = '-webkit-grabbing';
+
+  onmousemove = (e) => {
+    if (isDragging) {
+      let x = e.clientX - mx;
+      let y = e.clientY - my;
+
+      squarePos.x = x;
+      squarePos.y = y;
+
+      if (x < 0) {
+        squarePos.x = 0;
+      }
+      else if (x > bodyWidth() - sideLength) {
+        squarePos.x = bodyWidth() - sideLength;
+      }
+      if (y < 0) {
+        squarePos.y = 0;
+      }
+      else if (y > bodyHeight() - sideLength) {
+        squarePos.y = bodyHeight() - sideLength;
+      }
+
+      square.style.left = squarePos.x + 'px';
+      square.style.top = squarePos.y + 'px';
+    }
+  }
 }
 
-onmousemove = (e) => {
+document.body.onmouseup = () => {
+  isDragging = false;
+  square.style.cursor = '-webkit-grab';
+}
+
+square.onmouseover = () => {
+  square.style.cursor = '-webkit-grab';
   if (isDragging) {
-    dragSquare(e.clientX, e.clientY);
+    square.style.cursor = '-webkit-grabbing';
   }
 }
